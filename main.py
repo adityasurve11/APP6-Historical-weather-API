bb01cea62d07e9e7bd7ebeea49ea3ea512d2f63e
ADITYA <aditya11surve08@gmail.com>
2024-09-27 22:50:08 +0530

    still gettting error

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask("Website")
 def home():
     return render_template("Tutorial.html")
 
-@app.route("/about/")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
