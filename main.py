502c8ccf89838b559d4a891aad764cc2dbff6545
ADITYA <aditya11surve08@gmail.com>
2024-09-29 23:48:20 +0530

    make a .gitignore file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -12,6 +14,7 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
