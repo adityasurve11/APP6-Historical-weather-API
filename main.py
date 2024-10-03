f92d7a5ac5a834ba459119d06000aa2299865322
ADITYA <aditya11surve08@gmail.com>
2024-10-03 13:52:22 +0530

    day 30: add URL endpoint for yearly and all data.

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[["STAID","STANAME                                 "]]
 
 
+
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
+
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -23,12 +25,14 @@ def about(station, date):
             "date": date,
             "temperature": temperature}
 
+
 @app.route("/api/v1/<station>")
 def all_data(station):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + " .txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     result = df.to_dict(orient="records")
     return result
+
 
 @app.route("/api/v1/yearly/<station>/<year>")
 def yearly(station, year):
